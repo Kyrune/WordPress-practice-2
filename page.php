b797ef15581883c47d95837159d53a7f4728be50
Kyrune <jn-01@centurytel.net>
2022-01-31 10:34:28 -0600

    Remove generic subtitle

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>Replace later</p>
         </div>
       </div>
     </div>
