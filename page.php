02705a7b0367d02dad069cf3af76dfd074242901
Kyrune <jn-01@centurytel.net>
2022-02-01 14:22:23 -0600

    Add page-links title dynamic title

--- page.php
+++ page.php
@@ -27,7 +27,7 @@
         ?>
 
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="#"></a><?php echo get_the_title($theParent); ?></h2>
         <ul class="min-list">
           <?php 
             if ($theParent) {
