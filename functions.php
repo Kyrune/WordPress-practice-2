8db75cb0040d5ff57790584a6d954e62f164635a
Kyrune <jn-01@centurytel.net>
2022-02-01 14:59:39 -0600

    Add footer menus

--- functions.php
+++ functions.php
@@ -14,6 +14,8 @@
     function university_features() {
         // Add header menu location in admin screen
         register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        register_nav_menu('footerLocationOne', 'Footer Location One');
+        register_nav_menu('footerLocationTwo', 'Footer Location Two');
         // Creates a title in the browser tab
         add_theme_support('title-tag');
     }
