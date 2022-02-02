deaa954e568529b073c47c1e87819d6703fb0d38
Kyrune <jn-01@centurytel.net>
2022-02-02 10:35:01 -0600

    Add permalink

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
       while(have_posts()) {
         the_post(); ?>
         <div class="post-item">
-          <h2><a href="#"><?php the_title(); ?></a></h2>
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         </div>
       <?php }
     ?>
