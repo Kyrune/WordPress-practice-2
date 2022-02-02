9bb80db96b222ecea7026f8243af8dd2a3cf70dd
Kyrune <jn-01@centurytel.net>
2022-02-02 11:22:30 -0600

    Add dynamic post date

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <div class="post-item">
           <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div class="metabox">
-              <p>Posted by <?php the_author_posts_link(); ?> on 6.8.17 in News</p>
+              <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in News</p>
           </div>
 
           <div class="generic-content">
