874dd1ec269f9e696e161146d8c04836425997b6
Kyrune <jn-01@centurytel.net>
2022-02-02 11:16:42 -0600

    Add h2 styling

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
       while(have_posts()) {
         the_post(); ?>
         <div class="post-item">
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div class="metabox">
               <p>Posted by Maria on 6.8.17 in News</p>
           </div>
