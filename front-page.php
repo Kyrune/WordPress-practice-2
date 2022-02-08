741bb0d74939cdf43daffec330d3f24419e08844
Kyrune <jn-01@centurytel.net>
2022-02-08 15:18:18 -0600

    Add dynamic event month date

--- front-page.php
+++ front-page.php
@@ -25,7 +25,11 @@
               $homepageEvents->the_post(); ?>
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month">Mar</span>
+                  <span class="event-summary__month"><?php 
+                    // Get event date
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                  ?></span>
                   <span class="event-summary__day">25</span>
                 </a>
                 <div class="event-summary__content">
