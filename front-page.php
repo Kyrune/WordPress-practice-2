5bfb14ad59aef6ac43c6da00af2aadba266117f7
Kyrune <jn-01@centurytel.net>
2022-02-08 11:18:37 -0600

    Add array to WP_Query

--- front-page.php
+++ front-page.php
@@ -16,7 +16,10 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php 
-            $homepageEvents = new WP_Query();
+            $homepageEvents = new WP_Query(array(
+              'posts_per_page' => 2,
+              'post_type' => 'event'
+            ));
           ?>
 
           <div class="event-summary">
