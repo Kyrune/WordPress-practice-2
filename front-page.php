11bfd8b621d3c1c6480c5728135407ecb8956e51
Kyrune <jn-01@centurytel.net>
2022-02-08 15:19:08 -0600

    Add dynamic event day date

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@
                     $eventDate = new DateTime(get_field('event_date'));
                     echo $eventDate->format('M');
                   ?></span>
-                  <span class="event-summary__day">25</span>
+                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny">
