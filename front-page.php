df97374f095fdad8688cfdf58ed58737b58990ec
Kyrune <jn-01@centurytel.net>
2022-02-08 11:29:42 -0600

    Add event title

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny">
-                    <a href="#">Poetry in the 100</a>
+                    <a href="#"><?php the_title(); ?></a>
                   </h5>
                   <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. 
                     <a href="#" class="nu gray">Learn more</a>
