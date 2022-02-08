04568012dcc720e44de77efe25995e99995aae00
Kyrune <jn-01@centurytel.net>
2022-02-08 11:28:16 -0600

    Add event summary to while-loop

--- front-page.php
+++ front-page.php
@@ -23,7 +23,16 @@
 
             while ($homepageEvents->have_posts()) {
               $homepageEvents->the_post(); ?>
-              <li><?php the_title(); ?></li>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="#">
+                  <span class="event-summary__month">Mar</span>
+                  <span class="event-summary__day">25</span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+                  <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
             <?php }
           ?>
 
