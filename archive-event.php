7376a0bfc6a8903750b21b6fd3d333e2f1dbda2d
Kyrune <jn-01@centurytel.net>
2022-02-08 11:50:08 -0600

    Add hardcode text for content

--- archive-event.php
+++ archive-event.php
@@ -7,14 +7,8 @@
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">All Events</h1>
-      <!-- <h1 class="page-banner__title"><?php if (is_category()) {
-          single_cat_title();
-      } 
-      if (is_author()) {
-          echo 'Posts by '; the_author();
-      } ?></h1> -->
       <div class="page-banner__intro">
-        <p><?php the_archive_description(); ?></p>
+        <p>See what is going on in our world.</p>
       </div>
     </div>
   </div>
