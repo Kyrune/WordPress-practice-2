b760bde6238e64b269766c768cc7dfe86711346d
Kyrune <jn-01@centurytel.net>
2022-02-15 11:40:19 -0600

    Add comment

--- front-page.php
+++ front-page.php
@@ -16,6 +16,7 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php 
+          // Custom Query
             $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
               'posts_per_page' => 2,
