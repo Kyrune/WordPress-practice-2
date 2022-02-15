67762c3518b07bfd13697ead6ed90017b0339850
Kyrune <jn-01@centurytel.net>
2022-02-15 11:33:28 -0600

    Add unordered list

--- single-event.php
+++ single-event.php
@@ -31,10 +31,12 @@
 
             <?php  
                 $relatedPrograms = get_field('related_programs');
-
+                
+                echo '<ul class="link-list min-list">';
                 foreach($relatedPrograms as $program) { ?>
                     <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
                 <?php }
+                echo '</ul>';
             ?>
         </div>
 
