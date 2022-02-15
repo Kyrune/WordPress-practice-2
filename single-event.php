83a109c36fcfe1506198be4e170cbbfb26207369
Kyrune <jn-01@centurytel.net>
2022-02-15 11:34:32 -0600

    Add h2 title

--- single-event.php
+++ single-event.php
@@ -32,6 +32,7 @@
             <?php  
                 $relatedPrograms = get_field('related_programs');
                 
+                echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
                 echo '<ul class="link-list min-list">';
                 foreach($relatedPrograms as $program) { ?>
                     <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
