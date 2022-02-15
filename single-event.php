4aad5e20028388a48420b8f8943248920b6a978f
Kyrune <jn-01@centurytel.net>
2022-02-15 11:35:27 -0600

    Add hr line

--- single-event.php
+++ single-event.php
@@ -32,6 +32,7 @@
             <?php  
                 $relatedPrograms = get_field('related_programs');
                 
+                echo '<hr class="section-break">';
                 echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
                 echo '<ul class="link-list min-list">';
                 foreach($relatedPrograms as $program) { ?>
