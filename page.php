055c79af37eee5af78c80061fceda475a1678eb2
Kyrune <jn-01@centurytel.net>
2022-02-17 10:44:02 -0600

    Add pageBanner function

--- page.php
+++ page.php
@@ -2,8 +2,9 @@
     get_header();
 
     while(have_posts()) {
-        the_post(); ?>
-
+        the_post(); 
+        pageBanner();
+        ?>
 
 
     <div class="container container--narrow page-section">
