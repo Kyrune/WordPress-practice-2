28f4b87ecdffcba89c4c5241f1ac5a516ff4249d
Kyrune <jn-01@centurytel.net>
2022-02-22 11:27:44 -0600

    Create Search class

---
+++ src/modules/Search.js
@@ -0,0 +1,18 @@
+class Search {
+    // 1. describe and create/initiate our object
+    constructor() {
+        
+    }
+    // 2. events 
+
+    // 3. methods (function, action...)
+    openOverlay() {
+
+    }
+
+    closeOverlay() {
+
+    }
+}
+
+export default Search
