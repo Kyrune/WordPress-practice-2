6d52189872537346abd2df3a9b3daac0724ff1ff
Kyrune <jn-01@centurytel.net>
2022-02-23 11:10:35 -0600

    Prevent keyCodes from running after keydown

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -7,6 +7,7 @@ class Search {
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
         this.events();
+        this.isOverlayOpen = false;
     }
 
     // 2. events 
@@ -18,12 +19,11 @@ class Search {
 
     // 3. methods (functions, actions...)
     keyPressDispatcher(e) {
-        
-        if (e.keyCode == 83) {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
             this.openOverlay();
         }
 
-        if (e.keyCode == 27) {
+        if (e.keyCode == 27 && this.isOverlayOpen) {
             this.closeOverlay();
         }
     }
@@ -31,11 +31,13 @@ class Search {
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass("search-overlay--active");
         $("body").removeClass("body-no-scroll");
+        this.isOverlayOpen = false;
     }
 }
 
