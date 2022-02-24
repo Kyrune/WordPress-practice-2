6d68b4e446b0be55738ead21898eeb9a3cc80b28
Kyrune <jn-01@centurytel.net>
2022-02-24 10:47:24 -0600

    Add if-statment for previousValue functionality

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -25,12 +25,16 @@ class Search {
 
     // 3. methods (functions, actions...)
     typingLogic() {
-        clearTimeout(this.typingTimer);
-        if (!this.isSpinnerVisible) {
-            this.resultsDiv.html('<div class="spinner-loader"></div>');
-            this.isSpinnerVisible = true;
+        if (this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if (!this.isSpinnerVisible) {
+                this.resultsDiv.html('<div class="spinner-loader"></div>');
+                this.isSpinnerVisible = true;
+            }
+            this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+
+        this.previousValue = this.searchField.val();
     }
 
     getResults() {
