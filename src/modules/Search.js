42df7820242525c14e609a0edae008b6cb827dec
Kyrune <jn-01@centurytel.net>
2022-03-01 13:51:15 -0600

    Add arrow function

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -46,8 +46,8 @@ class Search {
 
     // Get list of posts from live search
     getResults() {
-        $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-            
+        $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+            this.resultsDiv.html('');
         });
     }
 
