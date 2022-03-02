20392b00d043967b63a6cdb25d4683832df89a05
Kyrune <jn-01@centurytel.net>
2022-03-02 10:41:54 -0600

    Add addSearchHTML method

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -58,7 +58,7 @@ class Search {
     }
 
     keyPressDispatcher(e) {
-        if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
+        if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) { 
             this.openOverlay();
         }
 
@@ -77,6 +77,23 @@ class Search {
         this.searchOverlay.removeClass("search-overlay--active");
         $("body").removeClass("body-no-scroll");
         this.isOverlayOpen = false;
+    }
+
+    addSearchHTML() {
+        $("body").append(`
+            <div class="search-overlay">
+                <div class="search-overlay__top">
+                    <div class="container">
+                        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+                        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="container">
+                    <div id="search-overlay__results"></div>
+                </div>
+            </div>  
+        `);
     }
 }
 
