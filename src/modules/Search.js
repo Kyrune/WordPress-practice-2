9358023b20088e92f7da3aab6f5f0b9fb924c952
Kyrune <jn-01@centurytel.net>
2022-03-09 11:09:48 -0600

    Add item.permalink to event headline

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -91,7 +91,7 @@ class Search {
                                     </a>
                                     <div class="event-summary__content">
                                         <h5 class="event-summary__title headline headline--tiny">
-                                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                            <a href="${item.permalink}"><?php the_title(); ?></a>
                                         </h5>
                                         <p>
                                             <?php if (has_excerpt()) {
