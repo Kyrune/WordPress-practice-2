ebdd4586448cb3058e581b0c7afce3eb47faa91d
Kyrune <jn-01@centurytel.net>
2022-03-09 10:40:40 -0600

    Add item permalink

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -67,7 +67,7 @@ class Search {
                         ${results.professors.length ? '<ul class="professor-cards">' : `<p>No professors match that search.</p>`}
                             ${results.professors.map(item => `
                                 <li class="professor-card__list-item">
-                                    <a class="professor-card" href="<?php the_permalink(); ?>">
+                                    <a class="professor-card" href="${item.permalink}">
                                         <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
                                         <span class="professor-card__name"><?php the_title(); ?></span>
                                     </a>
