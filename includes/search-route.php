9c3ce87d439e3f3860b256d11d9e9d5925dc06c3
Kyrune <jn-01@centurytel.net>
2022-03-09 15:03:55 -0600

    Add programsMetaQuery with a foreach loop

--- includes/search-route.php
+++ includes/search-route.php
@@ -83,16 +83,20 @@
             }
         }
 
+        $programsMetaQuery = array('relation' => 'OR');
+
+        foreach($results['programs'] as $item) {
+            array_push($programsMetaQuery, array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . $item['id'] . '"'
+            ));
+        }
+
         // Custom query for program relationships
         $programRelationshipQuery = new WP_Query(array(
             'post_type' => 'professor',
-            'meta_query' => array(
-                array(
-                    'key' => 'related_programs',
-                    'compare' => 'LIKE',
-                    'value' => '"' . $results['programs'][0]['id'] . '"'
-                )
-            )
+            'meta_query' => $programsMetaQuery
         ));
 
         while($programRelationshipQuery->have_posts()) {
