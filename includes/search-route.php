50508b7a0b6da6e7821badf4263cace4db03cd1d
Kyrune <jn-01@centurytel.net>
2022-03-09 15:12:21 -0600

    Move programs code into if-statement

--- includes/search-route.php
+++ includes/search-route.php
@@ -83,36 +83,39 @@
             }
         }
 
-        $programsMetaQuery = array('relation' => 'OR');
-
-        foreach($results['programs'] as $item) {
-            array_push($programsMetaQuery, array(
-                'key' => 'related_programs',
-                'compare' => 'LIKE',
-                'value' => '"' . $item['id'] . '"'
-            ));
-        }
-
-        // Custom query for program relationships
-        $programRelationshipQuery = new WP_Query(array(
-            'post_type' => 'professor',
-            'meta_query' => $programsMetaQuery
-        ));
-
-        while($programRelationshipQuery->have_posts()) {
-            $programRelationshipQuery->the_post();
-
-            if (get_post_type() == 'professor') {
-                // Add array to post
-                array_push($results['professors'], array(
-                    'title' => get_the_title(),
-                    'permalink' => get_the_permalink(),
-                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        if ($results['programs']) {
+            $programsMetaQuery = array('relation' => 'OR');
+    
+            foreach($results['programs'] as $item) {
+                array_push($programsMetaQuery, array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . $item['id'] . '"'
                 ));
             }
+    
+            // Custom query for program relationships
+            $programRelationshipQuery = new WP_Query(array(
+                'post_type' => 'professor',
+                'meta_query' => $programsMetaQuery
+            ));
+    
+            while($programRelationshipQuery->have_posts()) {
+                $programRelationshipQuery->the_post();
+    
+                if (get_post_type() == 'professor') {
+                    // Add array to post
+                    array_push($results['professors'], array(
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink(),
+                        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                    ));
+                }
+            }
+    
+            $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
         }
 
-        $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
 
         return $results;
     }
