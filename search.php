331b60ca10c4b58846be2838b754b297a8d28d56
Kyrune <jn-01@centurytel.net>
2022-03-10 14:00:02 -0600

    Add if-statement for no posts

--- search.php
+++ search.php
@@ -10,13 +10,16 @@
 
   <div class="container container--narrow page-section">
     <?php
-      while(have_posts()) {
-        the_post(); 
-        get_template_part('template-parts/content', get_post_type());
-        ?>
-
-      <?php }
+    if (have_posts()) {
+        while(have_posts()) {
+          the_post(); 
+          get_template_part('template-parts/content', get_post_type());
+  
+        }
         echo paginate_links();
+    } else {
+        echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+    }
     ?>
   </div>
 
