ac08b5fece90a0099e4b32386954f9bcad8612b7
Kyrune <jn-01@centurytel.net>
2022-03-10 11:25:35 -0600

    Add template part

--- search.php
+++ search.php
@@ -11,7 +11,9 @@
   <div class="container container--narrow page-section">
     <?php
       while(have_posts()) {
-        the_post(); ?>
+        the_post(); 
+        get_template_part('template-parts/content', get_post_type());
+        ?>
         <div class="post-item">
           <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div class="metabox">
