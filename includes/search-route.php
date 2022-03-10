797fc95bdd2cb6d12ccd9a24dc7f3c6392f3e213
Kyrune <jn-01@centurytel.net>
2022-03-10 09:35:58 -0600

    Add events to results to remove duplicates

--- includes/search-route.php
+++ includes/search-route.php
@@ -133,6 +133,7 @@
     
             // Removes duplicates
             $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+            $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
         }
 
 
