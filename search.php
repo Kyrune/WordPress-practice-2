9aa6541f52f836d9c6fda23f113d79216d241c40
Kyrune <jn-01@centurytel.net>
2022-03-10 11:27:52 -0600

    Move div to template parts

--- search.php
+++ search.php
@@ -14,18 +14,7 @@
         the_post(); 
         get_template_part('template-parts/content', get_post_type());
         ?>
-        <div class="post-item">
-          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <div class="metabox">
-              <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> 
-              in <?php echo get_the_category_list(', '); ?></p>
-          </div>
 
-          <div class="generic-content">
-            <?php the_excerpt(); ?>
-            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-          </div>
-        </div>
       <?php }
         echo paginate_links();
     ?>
