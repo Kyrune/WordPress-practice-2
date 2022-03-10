f2cd135d00e0cd6486f2b6ef1758c448d689c5d6
Kyrune <jn-01@centurytel.net>
2022-03-10 09:35:02 -0600

    Add if-statement for event properties

--- includes/search-route.php
+++ includes/search-route.php
@@ -102,6 +102,24 @@
     
             while($programRelationshipQuery->have_posts()) {
                 $programRelationshipQuery->the_post();
+
+                if (get_post_type() == 'event') {
+                    $eventDate = new DateTime(get_field('event_date'));
+                    $description = null;
+                    if (has_excerpt()) {
+                        $description = get_the_excerpt();
+                    } else {
+                        $description = wp_trim_words(get_the_content(), 18);
+                    }
+                    // Add array to post
+                    array_push($results['events'], array(
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink(),
+                        'month' => $eventDate->format('M'),
+                        'day' => $eventDate->format('d'),
+                        'description' => $description
+                    ));
+                }
     
                 if (get_post_type() == 'professor') {
                     // Add array to post
