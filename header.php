215c978cb0194c5fa20a9cd84379a0999b1b9674
Kyrune <jn-01@centurytel.net>
2022-03-10 10:17:24 -0600

    Change span tag to a

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <h1 class="school-logo-text float-left">
           <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
         </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <a class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
