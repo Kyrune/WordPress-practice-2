c93ed16898bb5bf466d4d8930819f463e82a20e9
Kyrune <jn-01@centurytel.net>
2022-03-10 10:30:19 -0600

    Add classes to inputs

--- page-search.php
+++ page-search.php
@@ -50,8 +50,8 @@
         <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
             <label class="headline headline--medium" for="s">Perform a New Search:</label>
             <div class="search-form-row">
-                <input id="s" type="search" name="s">
-                <input type="submit" value="Search">
+                <input class="s" id="s" type="search" name="s">
+                <input class="search-submit" type="submit" value="Search">
             </div>
         </form>
       </div>
