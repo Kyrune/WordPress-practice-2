493e914d75503a69bee9261c1dccb1029648dcc9
Kyrune <jn-01@centurytel.net>
2022-03-10 14:05:38 -0600

    Add comment

--- searchform.php
+++ searchform.php
@@ -1,3 +1,4 @@
+<!-- esc_url = security best practice -->
 <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
     <label class="headline headline--medium" for="s">Perform a New Search:</label>
     <div class="search-form-row">
