411ed4412e1e4d949fb164704ff63e01754ce119
Kyrune <jn-01@centurytel.net>
2022-03-16 11:01:59 -0500

    Create ourLoginCSS function

--- functions.php
+++ functions.php
@@ -136,6 +136,13 @@
 
     add_action('login_enqueue_scripts', 'ourLoginCSS');
 
+    function ourLoginCSS() {
+        wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+        wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    }
+
     // Adds post type of Events
     // function university_post_types() {
     //     register_post_type('event', array(
