5998021e39519a95e3957a8073ad44fd618e3bf6
Kyrune <jn-01@centurytel.net>
2022-03-17 11:29:33 -0500

    Add thisNote as parameter in makeNoteEditable/makeNoteReadOnly functions

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -16,21 +16,21 @@ class MyNotes {
         var thisNote = $(e.target).parents("li");
         if (thisNote.data("state") == "editable") {
             // Make read only
-            this.makeNoteReadOnly();
+            this.makeNoteReadOnly(thisNote);
         } else {
             // Make editable
-            this.makeNoteEditable();
+            this.makeNoteEditable(thisNote);
         }
     }
 
-    makeNoteEditable() {
+    makeNoteEditable(thisNote) {
         thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
         thisNote.find(".update-note").addClass("update-note--visible");
         thisNote.data("state", "editable");
     }
 
-    makeNoteReadOnly() {
+    makeNoteReadOnly(thisNote) {
         thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
         thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field");
         thisNote.find(".update-note").removeClass("update-note--visible");
