4741396b0605b2a6d1caffc1aa36dee90ce77937
Kyrune <jn-01@centurytel.net>
2022-03-17 10:08:39 -0500

    Create deleteNote function

--- src/modules/my-notes.js
+++ src/modules/my-notes.js
@@ -2,14 +2,18 @@ import $ from 'jquery';
 class MyNotes {
     constructor() {
         // alert("Hello from MyNotes");
+        this.events();
     }
 
 
     events() {
-
+        $(".delete-note").on("click", this.deleteNote);
     }
 
     // Custom methods
+    deleteNote() {
+        // alert("you clicked delete");
+    }
 }
 
 export default MyNotes;
