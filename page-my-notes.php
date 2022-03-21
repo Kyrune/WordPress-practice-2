0be08d3231de913d584c94eaf01141a5c2cab07c
Kyrune <jn-01@centurytel.net>
2022-03-21 14:40:48 -0500

    Move note limit span to create-note div

--- page-my-notes.php
+++ page-my-notes.php
@@ -19,6 +19,7 @@
                 <input class="new-note-title" placeholder="Title">
                 <textarea class="new-note-body" placeholder="Your note here..."></textarea>
                 <span class="submit-note">Create Note</span>
+                <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
             </div>
 
             <ul class="min-list link-list" id="my-notes">
@@ -37,7 +38,6 @@
                             <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
                             <textarea readonly class="note-body-field"><?php echo esc_textarea(wp_strip_all_tags(get_the_content())); ?></textarea>
                             <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
-                            <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
                         </li>
                     <?php }
 
