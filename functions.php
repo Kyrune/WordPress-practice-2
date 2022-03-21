cc9f361e5e1757606081c32810e5b3040a5dd564
Kyrune <jn-01@centurytel.net>
2022-03-21 14:18:52 -0500

    Add if-statement to limit notes per user

--- functions.php
+++ functions.php
@@ -159,6 +159,10 @@
     function makeNotePrivate($data) {
         // Ensures users notes are sanitized (removes html tags)
         if ($data['post_type'] == 'note') {
+            if (count_user_posts(get_current_user_id(), 'note') > 9) {
+                die("You have reached your note limit.");
+            }
+
             $data['post_content'] = sanitize_textarea_field($data['post_content']);
             $data['post_title'] = sanitize_text_field($data['post_title']);
         }
