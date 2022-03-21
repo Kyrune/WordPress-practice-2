0885c6ed0ad33fd2cdcdb624d088c506d4bad36f
Kyrune <jn-01@centurytel.net>
2022-03-21 14:10:21 -0500

    Add sanitization to post title in makeNotePrivate function

--- functions.php
+++ functions.php
@@ -160,6 +160,7 @@
         // Ensures users notes are sanitized (removes html tags)
         if ($data['post_type'] == 'note') {
             $data['post_content'] = sanitize_textarea_field($data['post_content']);
+            $data['post_title'] = sanitize_text_field($data['post_title']);
         }
 
         if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
