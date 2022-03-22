201038a78526e9ecd5de1211d011e63aefbbfc10
Kyrune <jn-01@centurytel.net>
2022-03-22 10:29:09 -0500

    Add like-count span

--- single-professor.php
+++ single-professor.php
@@ -19,6 +19,7 @@
                         <span class="like-box">
                             <i class="fa fa-heart-o" aria-hidden="true"></i>
                             <i class="fa fa-heart" aria-hidden="true"></i>
+                            <span class="like-count"></span>
                         </span>
                         <?php the_content(); ?>
                     </div>
