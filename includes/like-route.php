f529547c91721908e4f11e7e4546712ebc14560d
Kyrune <jn-01@centurytel.net>
2022-03-22 13:48:35 -0500

    Add universityLikeRoutes function

--- includes/like-route.php
+++ includes/like-route.php
@@ -1,3 +1,17 @@
 <?php
 
 add_action('rest_api_init', 'universityLikeRoutes');
+
+function universityLikeRoutes() {
+    register_rest_route('university/v1', 'manageLike', array(
+        'methods' => 'POST',
+        'callback' => x
+    ));
+
+    register_rest_route('university/v1', 'manageLike', array(
+        'methods' => 'DELETE',
+        'callback' => x
+    ));
+}
+
+
