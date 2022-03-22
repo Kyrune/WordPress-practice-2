67b500877b468579c79913fdc252538cba3eaee4
Kyrune <jn-01@centurytel.net>
2022-03-22 13:49:36 -0500

    Add createLike/deleteLike to universityLikeRoutes

--- includes/like-route.php
+++ includes/like-route.php
@@ -5,12 +5,12 @@ add_action('rest_api_init', 'universityLikeRoutes');
 function universityLikeRoutes() {
     register_rest_route('university/v1', 'manageLike', array(
         'methods' => 'POST',
-        'callback' => x
+        'callback' => 'createLike'
     ));
 
     register_rest_route('university/v1', 'manageLike', array(
         'methods' => 'DELETE',
-        'callback' => x
+        'callback' => 'deleteLike'
     ));
 }
 
@@ -19,5 +19,5 @@ function createLike() {
 }
 
 function deleteLike() {
-    
+
 }
